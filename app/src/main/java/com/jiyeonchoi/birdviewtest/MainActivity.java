457dc22675fedd55b697b44d93e9d4d3cd8116b4
jiyeon <choi02647@naver.com>
2019-08-20 17:05:56 +0900

    test

--- app/src/main/java/com/jiyeonchoi/birdviewtest/MainActivity.java
+++ app/src/main/java/com/jiyeonchoi/birdviewtest/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //testee
+        //testeeddd
         TextView text1 = findViewById(R.id.text100);
 
     }
