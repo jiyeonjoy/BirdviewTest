9a375272259463c604cb3124ef686b98c972a5b5
jiyeon <choi02647@naver.com>
2019-08-21 11:09:37 +0900

    dataBinding

--- app/src/main/java/com/jiyeonchoi/birdviewtest/MainActivity.java
+++ app/src/main/java/com/jiyeonchoi/birdviewtest/MainActivity.java
@@ -3,8 +3,11 @@ package com.jiyeonchoi.birdviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jiyeonchoi.birdviewtest.databinding.ActivityMainBinding;
 
@@ -14,10 +17,28 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        ActivityMainBinding layout = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setMain(this);
+
+        binding.text100.setOnClickListener( v -> goHobbyListIntent(100));
+
+        binding.text10000.setOnClickListener( v -> goHobbyListIntent(10000));
+
+        binding.text500000.setOnClickListener( v -> goHobbyListIntent(50000));
+
+    }   // onCreate 끝
+
+
+    public void goHobbyListIntent(int peopleCount) {
+        Intent go_hobby_list = new Intent(getApplication().getApplicationContext(), MainActivity.class);
+        // 인텐트 값 넘기기
+        startActivity(go_hobby_list);
+        finish();
     }
+
 }
