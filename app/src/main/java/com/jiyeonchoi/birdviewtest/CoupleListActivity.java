268e4a524c720c281b64f0494e8bee02d7dc8231
jiyeon <choi02647@naver.com>
2019-08-21 14:04:56 +0900

    soojung

--- app/src/main/java/com/jiyeonchoi/birdviewtest/CoupleListActivity.java
+++ app/src/main/java/com/jiyeonchoi/birdviewtest/CoupleListActivity.java
@@ -1,14 +1,24 @@
 package com.jiyeonchoi.birdviewtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.jiyeonchoi.birdviewtest.databinding.ActivityCoupleListBinding;
+import com.jiyeonchoi.birdviewtest.databinding.ActivityMainBinding;
+
 public class CoupleListActivity extends AppCompatActivity {
+
+    private ActivityCoupleListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_couple_list);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_couple_list);
+        binding.setCoupleList(this);
+
+
     }
 }
--- app/src/main/java/com/jiyeonchoi/birdviewtest/HobbyListActivity.java
+++ app/src/main/java/com/jiyeonchoi/birdviewtest/HobbyListActivity.java
@@ -1,8 +1,13 @@
 package com.jiyeonchoi.birdviewtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.jiyeonchoi.birdviewtest.databinding.ActivityHobbyListBinding;
+import com.jiyeonchoi.birdviewtest.databinding.ActivityMainBinding;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -10,12 +15,19 @@ import java.io.InputStream;
 
 public class HobbyListActivity extends AppCompatActivity {
 
+    private ActivityHobbyListBinding binding;
+
+    int peopleCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hobby_list);
-        //TextView helloTxt = findViewById(R.id.textView1);
-        //helloTxt.setText(readTxt());
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_hobby_list);
+        binding.setHobbyList(this);
+
+        /*데이터 수신*/
+        Intent intent = getIntent();
+        peopleCount = intent.getExtras().getInt("peopleCount", 0);
     }
 
     private String readTxt() {
