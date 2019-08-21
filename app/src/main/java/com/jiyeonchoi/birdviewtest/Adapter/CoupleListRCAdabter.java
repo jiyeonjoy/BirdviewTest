539818ad5ea733c5023918b3c8645edcac10cc41
jiyeon <choi02647@naver.com>
2019-08-21 16:22:30 +0900

    adapter

--- app/src/main/java/com/jiyeonchoi/birdviewtest/Adapter/CoupleListRCAdabter.java
+++ app/src/main/java/com/jiyeonchoi/birdviewtest/Adapter/CoupleListRCAdabter.java
@@ -1,4 +1,67 @@
 package com.jiyeonchoi.birdviewtest.Adapter;
 
-public class CoupleListRCAdabter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.jiyeonchoi.birdviewtest.ListItem.CoupleListItem;
+import com.jiyeonchoi.birdviewtest.R;
+import java.util.ArrayList;
+
+public class CoupleListRCAdabter extends RecyclerView.Adapter<CoupleListRCAdabter.ViewHolder> {
+    Context context;
+    ArrayList<CoupleListItem> items;
+    int item_layout;
+
+    public CoupleListRCAdabter(Context context, ArrayList<CoupleListItem> items, int item_layout) {
+        this.context = context;
+        this.items = items;
+        this.item_layout=item_layout;
+    }
+
+    public void itemclear() {
+        items.clear();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.couple_list_item,parent,false);
+
+        return new ViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        final CoupleListItem item = items.get(position);
+
+        holder.coupleNumber.setText(item.getCoupleNumber());
+        holder.firstCouple.setText(item.getFirstCouple());
+        holder.secondCouple.setText(item.getSecondCouple());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView coupleNumber;
+        TextView firstCouple;
+        TextView secondCouple;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            coupleNumber = itemView.findViewById(R.id.coupleNumber);
+            firstCouple = itemView.findViewById(R.id.firstCouple);
+            secondCouple = itemView.findViewById(R.id.secondCouple);
+
+        }
+    }
 }
--- app/src/main/java/com/jiyeonchoi/birdviewtest/Adapter/HobbyListRCAdapter.java
+++ app/src/main/java/com/jiyeonchoi/birdviewtest/Adapter/HobbyListRCAdapter.java
@@ -1,4 +1,61 @@
 package com.jiyeonchoi.birdviewtest.Adapter;
 
-public class HobbyListRCAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.jiyeonchoi.birdviewtest.ListItem.HobbyListItem;
+import com.jiyeonchoi.birdviewtest.R;
+import java.util.ArrayList;
+
+public class HobbyListRCAdapter extends RecyclerView.Adapter<HobbyListRCAdapter.ViewHolder> {
+    Context context;
+    ArrayList<HobbyListItem> items;
+    int item_layout;
+
+    public HobbyListRCAdapter(Context context, ArrayList<HobbyListItem> items, int item_layout) {
+        this.context = context;
+        this.items = items;
+        this.item_layout=item_layout;
+    }
+
+    public void itemclear() {
+        items.clear();
+    }
+
+    @Override
+    public HobbyListRCAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.hobby_list_item,parent,false);
+
+        return new HobbyListRCAdapter.ViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(final HobbyListRCAdapter.ViewHolder holder, final int position) {
+        final HobbyListItem item = items.get(position);
+
+        holder.hobby.setText(item.getHobby());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView hobby;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            hobby = itemView.findViewById(R.id.hobby);
+
+        }
+    }
 }
+
